916815a3b13f385a2fef6bbb8c0560e01a36e73c
ImmanuelSindu <immanuel.sindu@ti.ukdw.ac.id>
2022-04-10 01:21:14 +0700

    update baru

--- LatihanPropertyAnimationStarter/app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/signup/SignupActivity.kt
+++ LatihanPropertyAnimationStarter/app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/signup/SignupActivity.kt
@@ -1,8 +1,11 @@
 package com.dicoding.picodiploma.loginwithanimation.view.signup
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import androidx.appcompat.app.AlertDialog
@@ -30,6 +33,30 @@ class SignupActivity : AppCompatActivity() {
         setupView()
         setupViewModel()
         setupAction()
+        playAnimation()
+    }
+
+    private fun playAnimation() {
+        ObjectAnimator.ofFloat(binding.imageView, View.TRANSLATION_X, -30f, 30f).apply {
+            duration = 6000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }.start()
+
+        val titleTextView = ObjectAnimator.ofFloat(binding.titleTextView, View.ALPHA, 1f).setDuration(500)
+        val nameTextView = ObjectAnimator.ofFloat(binding.nameTextView, View.ALPHA, 1f).setDuration(500)
+        val emailTextView = ObjectAnimator.ofFloat(binding.emailTextView, View.ALPHA, 1f).setDuration(500)
+        val passwordTextView = ObjectAnimator.ofFloat(binding.passwordTextView, View.ALPHA, 1f).setDuration(500)
+        val nameEditText = ObjectAnimator.ofFloat(binding.nameEditTextLayout, View.ALPHA, 1f).setDuration(500)
+        val emailEditText = ObjectAnimator.ofFloat(binding.emailEditTextLayout, View.ALPHA, 1f).setDuration(500)
+        val passwordEditText = ObjectAnimator.ofFloat(binding.passwordEditTextLayout, View.ALPHA, 1f).setDuration(500)
+        val signupButton = ObjectAnimator.ofFloat(binding.signupButton, View.ALPHA, 1F).setDuration(500)
+
+        AnimatorSet().apply{
+            playSequentially(titleTextView,nameTextView, nameEditText, emailTextView, emailEditText, passwordTextView, passwordEditText, signupButton)
+            start()
+        }
+
     }
 
     private fun setupView() {
